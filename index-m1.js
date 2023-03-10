0d7e2c49d4d6d2a3b2215b36a725a5255a51b4db
Vinicius Fujarra <107464091+befujarra@users.noreply.github.com>
2023-03-10 13:07:34 -0300

    Add files via upload

---
+++ index-m1.js
@@ -0,0 +1,25 @@
+document.getElementById("meuBotao").addEventListener("click", function (event) {
+  event.preventDefault();
+  let batidasForm = document.getElementById("batidas");
+  let batidasTotal = batidasForm.value;
+  let strips = (batidasTotal * 3) / 2;
+  let descarteForm = document.getElementById("descarte");
+  let pesoDescarte = descarteForm.value;
+  let descarteStrip = Math.round(pesoDescarte / 17.143);
+
+  document.getElementById("resultado").innerHTML =
+    "Total de strips: " +
+    strips +
+    "<br>" +
+    "Total de strips de descartados: " +
+    descarteStrip +
+    "<br>" +
+    "Total de Produção: " +
+    (strips - descarteStrip) +
+    "<br>" +
+    "Total de Descarte em Cartuchos: " +
+    Math.round(descarteStrip / 13) +
+    "<br>" +
+    "Total de Strips bons em Cartuchos: " +
+    Math.round((strips - descarteStrip) / 13);
+});
